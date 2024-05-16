1a18aee5d40594cdbe2951ee11f69ffd67530c52
José Luis Zúñiga <jose@tangibledesign.co>
2024-05-16 12:37:46 -0500

    Se agregan funciones para servicio de notificaciones

--- public/js/general.js
+++ public/js/general.js
@@ -3,22 +3,74 @@ $(() => {
   $('.tooltipped').tooltip({ delay: 50 })
   $('.modal').modal()
 
-  // TODO: Adicionar el service worker
-
-  // Init Firebase nuevamente
-
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  // const app = initializeApp(firebaseConfig);
-  // const analytics = getAnalytics(app);
 
-  // TODO: Registrar LLave publica de messaging
+  // Adicionar el service worker
+  navigator.serviceWorker.register('notificaciones-sw.js')
+  .then(registro => {
+    console.log(`Service worker registrado: ${registro}`);
+    firebase.messaging().useServiceWorker(registro)
+  })
+  .catch(error => {
+    console.error(`Error al registrar service worker: ${error}`)
+  });
 
-  // TODO: Solicitar permisos para las notificaciones
+  // Conectar con el proyecto de firebase para comunicar cambios
+  const messaging = firebase.messaging();
+  // Registrar LLave publica de messaging se genera con el proyecto
+  messaging.usePublicVapidKey(
+    'BOQ3tM61A8IhSvL4mzXypiFMFCsqy5GGxWlD5xy1oq1D8zWoC8JlFYz2nTTX7UiMCAvtzh3tk6DbSHE6-_0UKDg'
+  )
+  // Solicitar permisos para las notificaciones
+  messaging.requestPermission()
+  .then(() => {
+    // Si el usuario da permisos se resuelve esta promesa
+    // y me retorna un token de solicitud
+    console.log('Permisos de message otorgado');
+    return messaging.getToken()
+  })
+  .then(token => {
+    // Me suscribo a firestore
+    const db = firebase.firestore();
+    // Recibir datos en formato de fecha
+    //db.settings({timestampsInSnapshots: true})
 
+    // Vamos a la colección tokens, si no existe la crea
+    // y agregaremos el token que nos envian a la cola de tokens
+    // de esta manera se registra nuestro token para que al haber un
+    // un cambio nos de un mensaje de aviso
+    db.collection('tokens').doc(token).set({
+      token: token
+    }).catch(error => {
+      console.error(`Error al registrar el token: ${error}`);
+    });
+
+  })
+
+  // Refrescar el token
+  messaging.onTokenRefresh(()=> {
+    messaging.getToken()
+    .then((token) => {
+      console.log(`Token renovado`);
+      // Me suscribo a firestore
+      const db = firebase.firestore();
+      //Suscribo el token
+      db.collection('tokens').doc(token).set({
+        token: token
+      }).catch(error => {
+        console.error(`Error al registrar el token: ${error}`);
+      });
+    })
+  })
+  
   // TODO: Recibir las notificaciones cuando el usuario esta foreground
-
+  messaging.onMessage(payload => {
+    Materialize.toast(`Tenemos un nuevo Post ${payload.data.titulo}`, 6000);
+  })
+  
   // Recibir las notificaciones cuando el usuario esta background
+  // const analytics = getAnalytics(app);
   // Este es un observador para detectar cambios en los post
   const post = new Post();
   post.consultarTodosPost();
--- public/notificaciones-sw.js
+++ public/notificaciones-sw.js
@@ -1,6 +1,27 @@
-importScripts('https://www.gstatic.com/firebasejs/5.5.8/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/5.5.8/firebase-messaging.js')
+// Importa librerias necesarias para ejecutar en background
+importScripts('https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
+// Initialize Firebase
+firebase.initializeApp({
+  projectId: "blogmasterdata",
+  messagingSenderId: "311248035282",
+})
 
+const messagin = firebase.messagin();
 
-
+messagin.setBackgroundMessageHandles(payload => {
+  const tituloNotificacion = 'Ya tenemos una nueva notificación';
+  // Para crear la ventana de la notificación
+  const opcionesNotificacion = {
+    body: payload.data.titulo,
+    icon: 'icons/icon_new_post.png',
+    click_action: 'https://blogmasterdata.web.app/'
+  }
+  // Muestra la tarjeta con la información
+  // Esto es un servicio propio del browser
+  return self.registration.showNotificacion(
+    tituloNotificacion,
+    opcionesNotificacion
+  )
+})
